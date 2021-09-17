a8e6f454c301be33e71969c6a0be17421a11bf68
pieter bouwkamp <yspeedmail@imap.cc>
2021-09-17 17:53:40 +0200

    removing-comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
+
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
